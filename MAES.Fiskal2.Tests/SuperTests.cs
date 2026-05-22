236564c0ca1b34b2e507357e9216f9535af8de4e
Roko Tomović <roko@maesdbk.hr>
2026-05-22 14:50:47 +0200

    fiskal2 scaffold done

---
+++ MAES.Fiskal2.Tests/SuperTests.cs
@@ -0,0 +1,21 @@
+﻿using MAES.Fiskal2.Posrednici;
+
+namespace MAES.Fiskal2.Tests;
+
+public class SuperTests
+{
+    Super superProvider = new ()
+    {
+        BusinessGuid = Environment.GetEnvironmentVariable("SUPER_BUSINESS_GUID") ?? throw new InvalidOperationException("SUPER_BUSINESS_GUID environment variable is not set."),
+        Username = Environment.GetEnvironmentVariable("SUPER_USERNAME") ?? throw new InvalidOperationException("SUPER_USERNAME environment variable is not set."),
+        Password = Environment.GetEnvironmentVariable("SUPER_PASSWORD") ?? throw new InvalidOperationException("SUPER_PASSWORD environment variable is not set."),
+        IsDev = true
+    };
+
+    [Fact]
+    public async Task SendInvoiceUBL()
+    {
+        // dohvati ubl iz datoteke
+        await superProvider.EvidentirajUBLAsync("");
+    }
+}
